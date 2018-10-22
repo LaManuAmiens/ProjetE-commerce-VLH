fe04d4d02af18cf63f56385eac7f8a5139103181
Valentin <valentin.scellier@gmail.com>
2018-10-22 14:50:45 +0200

    boutton back to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,18 @@ $('#shoppingCart').on('click', '.removeItem', function(){
     $('#itemCount').css('display', 'none');
   }
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
